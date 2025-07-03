90fa75b33e3e27da72dcdb4006682343be833068
Navjot Kumar singh <navjotkumarsingh81@gmail.com>
2025-07-03 16:51:29 +0530

    methods01

--- 7.Method/Method01.java
+++ 7.Method/Method01.java
@@ -1,3 +1,12 @@
 public class Method01 {
-   
+   static void change(int A[],int index,int value){
+    A[index] = value;
+    }
+    public static void main(String[] args){
+        int A[] = {2,3,4,5};
+        change(A,2,20);
+        for(int x:A){
+            System.out.print(x + " ");
+        }
+    }
 }
